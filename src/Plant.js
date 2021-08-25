7ee0c2533cf043a94f8aee4082a5497cbe8b2126
Chris Ramer <chrisramer.97@gmail.com>
2021-08-25 09:19:17 -0700

    Functional approach

--- src/Plant.js
+++ src/Plant.js
@@ -5,15 +5,32 @@ class Plant {
 		this.light = 0;
 	}
 
-	hydrate() {
-		this.water++
-	}
-
 	feed() {
 		this.soil++
 	}
 
 	giveLight() {
 		this.light++
+	}
+}
+
+const hydrate = (plant) => {
+	return {
+		...plant,
+		water: (plant.water || 0) + 1
+	}
+}
+
+const feed = (plant) => {
+	return {
+		...plant,
+		soil: (plant.soil || 0) + 1
+	}
+}
+
+const giveLight = (plant) => {
+	return {
+		...plant,
+		light: (plant.light || 0) + 1
 	}
 }
