d5b90a89f59be9146a64392fd0e86b02e67cea71
Chris Ramer <chrisramer.97@gmail.com>
2021-08-25 09:23:29 -0700

    More abstract

--- src/Plant.js
+++ src/Plant.js
@@ -4,33 +4,9 @@ class Plant {
 		this.soil = 0;
 		this.light = 0;
 	}
-
-	feed() {
-		this.soil++
-	}
-
-	giveLight() {
-		this.light++
-	}
 }
 
-const hydrate = (plant) => {
-	return {
-		...plant,
-		water: (plant.water || 0) + 1
-	}
-}
-
-const feed = (plant) => {
-	return {
-		...plant,
-		soil: (plant.soil || 0) + 1
-	}
-}
-
-const giveLight = (plant) => {
-	return {
-		...plant,
-		light: (plant.light || 0) + 1
-	}
-}
+const changeState = (state, prop, value) => ({
+	...state,
+	[prop]: (state[prop] || 0) + value
+})
